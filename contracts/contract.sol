ef71398fef8b89376e6b2ede24ab95f1b20cdb63
Muhammad Waqar <MW6069605@GMAIL.COM>
2022-09-03 07:00:34 +0500

    Add files via upload

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -1,43 +1,18 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Counter{
-    string public name;
-    //store values for increase and decrease count(contract info)
-    uint public count; //unsigned integer means not negative
-    //public means available at public 
-  
-   //when its put on a blockchain constructor intializer for contract (runs only once)
-constructor (string memory _name, uint _intialcount){
-    name = _name; //underscore to deffrentiate vaiables
-    count= _intialcount;
-}
-//visibility using public 
-//increment function
-    function increment() public returns (uint newCount) {
-        count=count+1; //can use (count ++)
-        return count;
-    }
+contract MyContract {
+    string name = "My String";
 
-    //decrement function
-    function decrement() public returns (uint newCount){
-        count --;
-        return count;
+    function setName(string memory _name) public {
+        name = _name;
     }
-
-    //explicit function
-    //view read function from the chain but no modification at all 
-    //just reader function
-    function getCount() public view returns(uint){
-        return count;
-    }
-    function getName() public view returns(string memory currentName){
+    function getname() public view returns (string memory){
         return name;
     }
 
-    //update state function
-    //sets name
-    function setName(string memory _newName) public returns (string memory newName) {
-        name = _newName ;
-        return name;
+    function resetname() public {
+        name= "Waqar";
     }
 }
+
